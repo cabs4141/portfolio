5e9999fea0b23440bd039dbfd6c4c719cf1211a4
binda rama <crakun95@gmail.com>
2025-05-19 22:23:00 +0800

    fix ui

--- src/components/MyFooter/index.jsx
+++ src/components/MyFooter/index.jsx
@@ -1,17 +1,17 @@
 const MyFooter = () => {
   return (
     <div className="flex flex-col items-center justify-center bg-cookies h-auto gap-4 font-jetbrains bg-gray-800 text-white py-8 p-6 text-tiny xl:text-sm">
-      <p>© 2024 bindarama_. All rights reserved.</p>
+      <p>© 2025 All Rights Reserved.</p>
       <div className="container text-center border-t border-white" />
 
-      <div className="flex gap-8">
+      {/* <div className="flex gap-8">
         <div className="flex flex-col items-center justify-center gap-4 text-gray-400">
           <p>Terms and Condition.</p>
           <p>Privacy.</p>
           <p>Cookie Policy.</p>
           <p>Site Map.</p>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
